58c552e58b5a2d6a2e2f9e544698cfbe57a3c1f0
Matt Niehoff <nieho003@umn.edu>
2018-10-15 15:37:04 -0500

    End of module 3 code.
    Involved creating a Spike to verify our approach will work. Still work to do to make it more testable.

--- Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
+++ Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -1,7 +1,18 @@
-﻿namespace FriendStorage.UI.ViewModel
+﻿using System;
+
+namespace FriendStorage.UI.ViewModel
 {
   public class MainViewModel : ViewModelBase
   {
- 
-  }
+        public MainViewModel()
+        {
+            NavigationViewModel = new NavigationViewModel();
+        }
+
+        public NavigationViewModel NavigationViewModel { get; private set; }
+        public void Load()
+        {
+            NavigationViewModel.Load();
+        }
+    }
 }
--- Starter/FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
+++ Starter/FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FriendStorage.UI.ViewModel;
+using Xunit;
 
 namespace FriendStorage.UITests.ViewModel
 {
     public class NavigationViewModelTests
     {
+        [Fact]
+        public void ShouldLoadFriends()
+        {
+            var viewModel = new NavigationViewModel();
 
+            viewModel.Load();
+
+            // TODO: how do we figure out how to test this? example: get value to test without accessing file/web service.
+            // Assert.Equal(2, viewModel.Friends.Count);
+        }
     }
 }
