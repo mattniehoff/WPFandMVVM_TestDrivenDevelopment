2cb004fae0ab291dbb10c77cc22bd2c5005727df
Matt Niehoff <nieho003@umn.edu>
2018-10-16 13:31:21 -0500

    Creates a NavigationDataProvider class.

--- Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
+++ Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using FriendStorage.DataAccess;
+using FriendStorage.UI.DataProvider;
 
 namespace FriendStorage.UI.ViewModel
 {
-  public class MainViewModel : ViewModelBase
-  {
+    public class MainViewModel : ViewModelBase
+    {
         public MainViewModel()
-        {
-            // TODO: Fix this
-            // NavigationViewModel = new NavigationViewModel();
+        {           
+            NavigationViewModel =
+                new NavigationViewModel(
+                    new NavigationDataProvider(
+                        () => new FileDataService()));
         }
 
         public NavigationViewModel NavigationViewModel { get; private set; }
+
         public void Load()
         {
             NavigationViewModel.Load();
